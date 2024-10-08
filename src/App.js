eca0ff862832e5a6b7c3acd456b4670b0389f261
dayounglee <verdantjuly@kakao.com>
2024-10-08 11:50:07 +0900

    [Fear] 주석 추가 index.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
+    // jsx 내용
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
